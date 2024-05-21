3e4e8a5418c9c8572bbc59bd873d3f41c75de918
Rumon-Sarker <mdsrrumon1234@gmail.com>
2024-05-21 23:25:21 +0600

    users page add

---
+++ src/components/dashboard/adminHome/AdminHome.jsx
@@ -0,0 +1,10 @@
+
+const AdminHome = () => {
+    return (
+        <div>
+            <h1>Thsi is Admin Home Page</h1>
+        </div>
+    );
+};
+
+export default AdminHome;
---
+++ src/components/dashboard/users/Users.jsx
@@ -0,0 +1,10 @@
+
+const Users = () => {
+    return (
+        <div>
+            <h1>All Users </h1>
+        </div>
+    );
+};
+
+export default Users;
